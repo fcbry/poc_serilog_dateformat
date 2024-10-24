8d0467b0d376e593fd8418d74280f234ddb9d646
fcbry <bryan.hunt@foodsconnected.com>
2024-10-24 13:03:30 +0100

    examples of configuring date formatting (general,logging,appsettings)
    Signed-off-by: fcbry <bryan.hunt@foodsconnected.com>

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+using System.Globalization;
+
+namespace poc_serilog_dateformat
+{
+    internal class Program
+    {
+        private const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
+
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+
+            // DateTime format for log entries lines is set via the appsettings.json file
+            Log.Information("Hello, Serilog!");
+
+
+            // Custom DateTime format for log entries
+
+            var dateTime = DateTime.Now.ToString(DateTimeFormat);
+            Log.Information("DateTime custom formatted: " + dateTime);
+
+
+               
+
+            // Setting the global DataTime format
+            
+            CultureInfo customCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            customCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            customCulture.DateTimeFormat.LongTimePattern = "HH:mm:ss.fff";
+            CultureInfo.DefaultThreadCurrentCulture = customCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = customCulture;
+
+                             
+            Log.Information("DateTime global culture  : " + DateTime.Now.ToString());
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
